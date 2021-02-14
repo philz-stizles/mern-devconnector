786ea8417e2dbf5b246d046f17c3af2a7bc8ebf2
Theophilus Ighalo <theophilus.ighalo@venturegardengroup.com>
2021-02-14 20:12:57 +0100

    post model

--- models/Post.js
+++ models/Post.js
@@ -1,11 +1,109 @@
 const mongoose = require('mongoose');
 
 const PostSchema = new mongoose.Schema({
-    name: { type: String, required: true, trim: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    avatar: { type: String, required: true },
-    createAt: { type: Date, default: Date.now },
-});
+    company: {
+        type: String
+      },
+      website: {
+        type: String
+      },
+      location: {
+        type: String
+      },
+      status: {
+        type: String,
+        required: true
+      },
+      skills: {
+        type: [String],
+        required: true
+      },
+      bio: {
+        type: String
+      },
+      githubusername: {
+        type: String
+      },
+      experience: [
+        {
+          title: {
+            type: String,
+            required: true
+          },
+          company: {
+            type: String,
+            required: true
+          },
+          location: {
+            type: String
+          },
+          from: {
+            type: Date,
+            required: true
+          },
+          to: {
+            type: Date
+          },
+          current: {
+            type: Boolean,
+            default: false
+          },
+          description: {
+            type: String
+          }
+        }
+      ],
+      education: [
+        {
+          school: {
+            type: String,
+            required: true
+          },
+          degree: {
+            type: String,
+            required: true
+          },
+          fieldofstudy: {
+            type: String,
+            required: true
+          },
+          from: {
+            type: Date,
+            required: true
+          },
+          to: {
+            type: Date
+          },
+          current: {
+            type: Boolean,
+            default: false
+          },
+          description: {
+            type: String
+          }
+        }
+      ],
+      social: {
+        youtube: {
+          type: String
+        },
+        twitter: {
+          type: String
+        },
+        facebook: {
+          type: String
+        },
+        linkedin: {
+          type: String
+        },
+        instagram: {
+          type: String
+        }
+      },
+      date: {
+        type: Date,
+        default: Date.now
+      }
+    });
 
 module.exports = Post = mongoose.model('Posts', PostSchema);
